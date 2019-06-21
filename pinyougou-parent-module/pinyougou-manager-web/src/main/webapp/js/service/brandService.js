0ca174ace95486910508dd94393e899e9f51dc04
hefanyu <506189317@qq.com>
2019-06-21 14:26:40 +0800

    02.前端分层开发 03.代码分离JS

---
+++ pinyougou-parent-module/pinyougou-manager-web/src/main/webapp/js/service/brandService.js
@@ -0,0 +1,25 @@
+app.service('brandService',function ($http) {
+    this.search = function (currPage,pageSize,searchEntity) {
+        return $http.get('../brand/search.do?currPage=' + currPage + '&pageSize=' + pageSize,searchEntity)
+    };
+
+
+    this.save = function (entity) {
+        return $http.post('../brand/save.do', entity);
+    };
+
+    this.update = function (entity) {
+        return $http.post('../brand/update.do', entity);
+    };
+
+
+    this.findOne = function (id) {
+        return $http.get("../brand/findOne.do?id=" + id);
+    };
+
+
+    this.remove = function (selectIds) {
+        return $http.get("../brand/remove.do?ids=" + selectIds)
+    };
+
+});
