9434638e37106b392da4ec7a3ba3ca8be6b164ad
hefanyu <506189317@qq.com>
2019-06-21 18:08:23 +0800

    extend baseController

--- pinyougou-parent-module/pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
+++ pinyougou-parent-module/pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
@@ -1,24 +1,8 @@
-app.controller('brandController', function ($scope, $http,brandService) {
-    //查询所有
-    $scope.reloadList = function () {
-        $scope.search(
-            $scope.paginationConf.currentPage,
-            $scope.paginationConf.itemsPerPage
-        );
-        //每次删除操作后清空id数组
-        $scope.selectIds = [];
-    };
+app.controller('brandController', function ($scope,$controller,brandService) {
 
-    //分页属性配置
-    $scope.paginationConf = {
-        currentPage: 1,
-        totalItems: 10,
-        itemsPerPage: 8,
-        perPageOptions: [10, 20, 30, 40, 50],
-        onChange: function () {
-            $scope.reloadList();//重新加载
-        }
-    };
+    $controller('baseController',{$scope:$scope});
+
+
     //分页查询(已代替)
     /*$scope.findPage = function (currPage, pageSize) {
         $http.get('../brand/findByPage.do?currPage=' + currPage + '&pageSize=' + pageSize).success(
@@ -55,37 +39,6 @@ app.controller('brandController', function ($scope, $http,brandService) {
             }
         )
     };
-
-
-    //选中的id集合
-    $scope.selectIds = [];
-
-    //更新复选
-    $scope.updateSelection = function ($event, id) {
-        if ($event.target.checked) {
-            $scope.selectIds.push(id);//将选中id添加到集合
-        } else {
-            var idx = $scope.selectIds.indexOf(id);
-            $scope.selectIds.splice(idx, 1);//删除集合中非选id
-        }
-    };
-
-
-    //全选
-    $scope.selectAll = function ($event) {
-        if ($event.target.checked) {
-            $scope.isAllSelect = true;
-            $scope.isSelect = true;
-            angular.forEach($scope.list, function (entity, index) {
-                $scope.selectIds.push(entity.id);
-            });
-        } else {
-            $scope.isAllSelect = false;
-            $scope.isSelect = false;
-            $scope.selectIds = [];
-        }
-    };
-
 
     //批量删除
     $scope.remove = function () {
