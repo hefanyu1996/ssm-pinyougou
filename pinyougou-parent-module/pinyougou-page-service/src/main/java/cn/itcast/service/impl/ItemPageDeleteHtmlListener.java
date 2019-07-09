70894249ca9ba6dad51799c460e1486d8784c8dd
hefanyu <506189317@qq.com>
2019-07-09 17:33:55 +0800

    update pinyougou-manage-web activeMQ + debug 2019/7/9 commit

--- pinyougou-parent-module/pinyougou-page-service/src/main/java/cn/itcast/service/impl/ItemPageDeleteHtmlListener.java
+++ pinyougou-parent-module/pinyougou-page-service/src/main/java/cn/itcast/service/impl/ItemPageDeleteHtmlListener.java
@@ -1,12 +1,15 @@
 package cn.itcast.service.impl;
 
 import cn.itcast.service.ItemPageService;
+import com.alibaba.druid.filter.AutoLoad;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.jms.*;
 import java.io.Serializable;
 
 public class ItemPageDeleteHtmlListener implements MessageListener {
 
+    @Autowired
     private ItemPageService itemPageService;
 
     @Override
@@ -19,7 +22,7 @@ public class ItemPageDeleteHtmlListener implements MessageListener {
             Long[] ids = (Long[]) objectMessage.getObject();
 
             for (Long id : ids) {
-
+                System.out.println(id);
                 boolean flag = itemPageService.removeItemHtml(id);
 
                 System.out.println("同步删除商品详情页"+id+".html ："+flag);
